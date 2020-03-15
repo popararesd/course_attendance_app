397320579300422f10173a175deb87ac36a55cd8
popararesd <popararesd@gmail.com>
2020-03-15 19:30:05 +0200

    created course and students repo

--- demo/src/main/java/com/example/demo/entity/Student.java
+++ demo/src/main/java/com/example/demo/entity/Student.java
@@ -1,35 +1,24 @@
 package com.example.demo.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-public class Student {
+@DiscriminatorValue("student")
+public class Student extends User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @Column
-    private String name;
-    @Column
-    private String email;
-    @Column
-    private String phoneNumber;
     @Column
     private String registrationNumber;
     @Column
     private String identificationNumber;
 
     public Student (String name, String email, String phoneNumber,String registrationNumber,String identificationNumber){
-        this.name=name;
-        this.email=email;
-        this.phoneNumber=phoneNumber;
+        super(name,email,phoneNumber);
         this.registrationNumber=registrationNumber;
         this.identificationNumber=identificationNumber;
     }
 
-    public Student() {
-
-    }
+    public Student() {}
 
     public String getRegistrationNumber() {
         return registrationNumber;
@@ -46,29 +35,5 @@ public class Student {
     public void setIdentificationNumber(String identificationNumber) {
         this.identificationNumber = identificationNumber;
     }
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getId(){return this.id;}
 }
